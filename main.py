59144dc4e4c558f8c9f4c7237f0e63c92b687c84
gudmunduro <goh00@protonmail.com>
2020-09-17 09:38:55 +0000

    Add base functions

--- main.py
+++ main.py
@@ -26,3 +26,13 @@ ROW1 = "N|NES|E"
 ROW2 = "N|WS|EW"
 ROW3 = "V|NS|WS"
 
+def print_directions(indexX, row):
+    pass
+
+def can_move_to_direction(indexX, row, direction):
+    pass
+
+def move_to_direction(indexX, row, direction):
+    pass
+
+
