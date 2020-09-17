7baa0f6ab5301a5572dd538e8265c1c7016af98a
jonasfreyr <jonasfreyrbjarnason@gmail.com>
2020-09-17 09:30:53 +0000

    Adda grunn fyrir algríminn

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+# # # # # # #
+#   #   #   #
+# # # _ # # #
+#   #   |   #
+# # | # | # #
+#   |   |   #
+# # # # # # #
+
+
+
+indexX = 1
+indexY = 1
+
+ROW1 = "N|NES|E"
+ROW2 = "N|WS|EW"
+ROW3 = "V|NS|WS"
+
+Notum strengi til að halda utan um rows og erum með tvö index
+Notum síðan | til að skipta upp ROW í cells
+'''
+
+indexX = 1
+indexY = 1
+ROW1 = "N|NES|E"
+ROW2 = "N|WS|EW"
+ROW3 = "V|NS|WS"
+
